8b27ae93726e20be83aa4086fd32b7168d09881d
charnchol <khumball46@gmail.com>
2022-02-03 16:24:48 +0700

    lab 22 complete

--- lab22.h
+++ lab22.h
@@ -63,13 +63,37 @@ void Unit::newTurn(){
 	guard_on = false;
 }
 
+bool Unit::isDead(){
+	if(hp <= 0) return true;
+	else return false;
+}
 
+void Unit::guard(){
+	guard_on = true;
+}
 
-/////////////////////////////////////////////////////////////////////////////////////
-//Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
-/////////////////////////////////////////////////////////////////////////////////////
+int Unit::beAttacked(int oppatk){
+	int dmg;
+	if(guard_on == true){
+		dmg = (oppatk - def)/3;
+	}else{
+		dmg = oppatk - def;
+	}
+	if(dmg > 0) hp -= dmg;
+	return dmg;
+}
 
+int Unit::attack(Unit &target){
+	return target.beAttacked(atk);
+}
 
+int Unit::heal(){
+	int a = hpmax - hp;
+	int random = rand()%21 + 10;
+	if(random >= a) random = a;
+	hp += random;
+	return random;
+}
 
 void drawScene(char p_action,int p,char m_action,int m){
 	cout << "                                                       \n";
